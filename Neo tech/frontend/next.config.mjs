7a12de776108251f871922482673efa156304d9c
Gradi <tshiabutagradi@gmail.com>
2025-05-02 00:21:34 +0200

    unoptimized images

--- Neo tech/frontend/next.config.mjs
+++ Neo tech/frontend/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   devIndicators: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
